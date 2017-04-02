1b6791cb3f27a3101176bf7ca97e0b3c0f23366f
Abdullah Rahman <abdullah2891@gmail.com>
2017-04-02 15:37:26 +0000

    commit loading model

--- app/routes/issues.js
+++ app/routes/issues.js
@@ -4,14 +4,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(){
-        var url = "https://perl-workspace-abdullah2891.c9users.io/issue"; 
-        return  Ember.$.getJSON(url).then(function(dataObj){
-        
-            var list = Object.keys(dataObj.response.Issue).map(function(issue){
-                return dataObj.response.Issue[issue];
-            });
-            console.log(list);
-            return list;
-        });
+        var issue = this.get('store').findAll('issue');
+        console.log(issue);
+        return issue;
     }
 });
