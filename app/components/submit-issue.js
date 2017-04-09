bd8b86987b4d61c6b3e63dd2464f0873b9e3983b
Abdullah Rahman <abdullah2891@gmail.com>
2017-04-09 14:55:19 +0000

    adding issue from the frontend

--- app/components/submit-issue.js
+++ app/components/submit-issue.js
@@ -2,18 +2,33 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     
-    store : Ember.inject.service(), 
+    store : Ember.inject.service(),
+    IssueTitle : '',
     actions:{
         submit : function(){
                 console.log("submitting");
-                var store = this.get('store'); 
+                var store = this.get('store');
+                
+                
+                var 
+                    IssueTitle = this.get('IssueTitle'),
+                    IssueDescription = this.get('IssueDescription'),
+                    IssueOwner = this.get('IssueOwner')
+                ;
+
+                
+                if(!(IssueTitle && IssueDescription && IssueOwner)){
+                    console.log("Failed to submit");
+                    return;   
+                }
+                
                 // console.log("store ",store);
                 var issue = store.createRecord('issue',
                 {
-                    "title": "creating issue hard coded", 
-                    "description" : "hardcoding issue", 
-                    "owner" : "lame coder", 
-                    "status" : "open"
+                    "title": IssueTitle, 
+                    "description" :IssueDescription, 
+                    "owner" : IssueOwner, 
+                    "status" : "open" //new issues are always should be open
                 });
                 issue.save();
         }
