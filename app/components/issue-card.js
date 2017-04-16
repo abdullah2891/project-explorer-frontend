06447e09f3cede083b2316a00ad8c5f08e833233
Abdullah Rahman <abdullah2891@gmail.com>
2017-04-16 01:22:26 +0000

    Squashed commit of the following:
    commit 1b01c962e62d1c017fe449c6efb948465aafde2c
    Author: Abdullah Rahman <abdullah2891@gmail.com>
    Date:   Sun Apr 16 01:21:09 2017 +0000
    
        changed the status hold to working
    
    commit 5078f5a7e91ac31270848188e47ee2af5673741f
    Author: Abdullah Rahman <abdullah2891@gmail.com>
    Date:   Sat Apr 15 20:11:57 2017 +0000
    
        changing the status

--- app/components/issue-card.js
+++ app/components/issue-card.js
@@ -1,4 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    store : Ember.inject.service(),
+
+    actions:{
+        updateIssue : function(id){
+            var store = this.get('store'); 
+            // linked list to change status
+            var statusChange = {
+                    "open" : "working", 
+                    "working" : "closed", 
+                    "closed" : "open"
+            };
+            
+            
+            
+            store.find('issue',id).then(function(issue){
+                console.log("findRecord working?");
+                console.log(issue);
+                var status = issue.get('status'); 
+                console.log(status) ; 
+                issue.set('status', statusChange[status]); 
+                issue.save().then(function(response){
+                    console.log(response);
+                })
+            });
+            
+            
+            console.log("clicked",id);
+        }
+    }
 });
--- app/helpers/compare-string.js
+++ app/helpers/compare-string.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export function compareString(params/*, hash*/) {
-    return params[0].trim() === params[1].trim();
+    if(params.length==3){
+        return params[0] != params[1];    
+    }
+    return params[0] === params[1];
 }
 
 export default Ember.Helper.helper(compareString);
